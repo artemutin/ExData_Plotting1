cdd99bdfc465a3111a7b6a767e352c88fad0e8ab
= <artemutin@yandex.ru>
2014-07-12 12:44:37 +1100

    plot4

--- plot4.r
+++ plot4.r
@@ -11,11 +11,19 @@ used_data$Time <- strptime(used_data$Time, format="%d/%m/%Y %H:%M:%S")
 #setting english locale for correct english weekdays labels
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
 #setting output device
-png(filename="~/R/scripts/plotting_asignment/plot3.png")
+png(filename="~/R/scripts/plotting_asignment/plot4.png")
 #plotting =)
+par(mfrow=c(2,2))
+#plot NW
+plot(used_data$Time, used_data$Global_active_power, xlab="", ylab="Global Active Power", type="l")
+#plot NE
+plot(used_data$Time, used_data$Voltage, xlab="datetime", ylab="Voltage", type="l")
+#plot SW
 plot(used_data$Time, used_data$Sub_metering_1, type="n", xlab="", ylab = "Energy Sub Metering")
 lines(used_data$Time, used_data$Sub_metering_1,  col="black")
 lines(used_data$Time, used_data$Sub_metering_2,  col="red")
 lines(used_data$Time, used_data$Sub_metering_3,  col="blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lwd=1)
+legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lwd=1, bty="n")
+#plot SE
+plot(used_data$Time, used_data$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type = "l")
 dev.off()
